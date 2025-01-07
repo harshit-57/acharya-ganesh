458ab7e9cbd3184e3b9c24e818bf9d16e85db81d
Gaurav Saini <codegsaini@gmail.com>
2025-01-08 01:06:03 +0530

    feat: Contact and About part 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import BlogList from './pages/blog/BlogList';
 import CoursesList from './pages/courses/CoursesList';
+import Contact from './pages/contact/Contact';
+import AboutUs from './pages/about/AboutUs';
 
 export default () => {
     return (
@@ -9,6 +11,8 @@ export default () => {
             <Route index path="/" element={<Home />} />
             <Route path="/blog/:category" element={<BlogList />} />
             <Route path="/courses" element={<CoursesList />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<Navigate to={'/'} />} />
         </Routes>
     );
---
+++ src/pages/about/AboutUs.jsx
@@ -0,0 +1,37 @@
+import css from './style.module.css';
+import { PageContainer } from '../../components/page-container/PageContainer';
+import TopBar from '../home/components/hero/components/top-bar/TopBar';
+import Navigation from '../home/components/hero/components/navigation/Navigation';
+import Footer from '../footer/Footer';
+import IcChevronIcon from '../../assets/chevron-down.png';
+
+import ImgHeaderBg from '../../assets/contact_header_bg.png';
+
+const AboutUs = () => {
+    return (
+        <PageContainer className={css.container}>
+            <div
+                style={{ backgroundImage: `url(${ImgHeaderBg})` }}
+                className={css.header}
+            >
+                <TopBar />
+                <Navigation />
+                <div className={css.header_text_container}>
+                    <div className={css.header_text_wrapper}>
+                        <h3>About Us</h3>
+                        <p>
+                            <span>Home</span>{' '}
+                            <span>
+                                <img src={IcChevronIcon} alt={''} />
+                            </span>{' '}
+                            <span>About us</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </PageContainer>
+    );
+};
+
+export default AboutUs;
