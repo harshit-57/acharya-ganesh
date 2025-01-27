6e467ebcbe582967854e4487859b7c203bdf9735
Gaurav Saini <codegsaini@gmail.com>
2025-01-27 22:16:07 +0530

    services screens

--- src/pages/services/wealth-astrology/WealthAstrology.jsx
+++ src/pages/services/wealth-astrology/WealthAstrology.jsx
@@ -15,7 +15,354 @@ export const WealthAstrology = () => {
                 <TopBar />
                 <Navigation />
             </div>
-            <div className={css.content_container}></div>
+            <div className={css.content_container}>
+                <div className={css.content_wrapper}>
+                    <div
+                        className={css.column}
+                        style={{ textAlign: 'center', fontSize: '18px' }}
+                    >
+                        <h1 style={{ color: '#9a5c23', paddingBottom: '20px' }}>
+                            Our Wealth Astrology
+                        </h1>
+                        <p style={{ textAlign: 'start' }}>
+                            The desire for wealth and prosperity has always
+                            piqued astrologers’ interest and curiosity. Acharya
+                            Ganesh’s Wealth astrology is based on the notion
+                            that the placements and movements of celestial
+                            bodies can impact an individual’s financial
+                            well-being. Our astrology can provide insights into
+                            personality qualities and life events, but it is
+                            also said to predict one’s financial success.
+                        </p>
+                        <h1 style={{ color: '#9a5c23', paddingBottom: '20px' }}>
+                            Services at Acharya Ganesh
+                        </h1>
+                        <p style={{ textAlign: 'start' }}>
+                            Welcome to Acharya Ganesh, your premier destination
+                            for exploring the mystical realms of astrology and
+                            uncovering the secrets of the cosmos. Our astrology
+                            services are designed to offer profound insights
+                            into your life’s journey, empowering you with the
+                            wisdom to navigate your path with clarity and
+                            purpose. Dive into our world, where ancient wisdom
+                            meets modern understanding, and discover how the
+                            stars and planets align to influence your destiny.
+                        </p>
+                    </div>
+                    <div
+                        className={css.column}
+                        style={{ textAlign: 'center', color: '#9a5c23' }}
+                    >
+                        <h1>Wealth Astrology</h1>
+                        <h1>The Role of Money Attract Symbol</h1>
+                    </div>
+                    <div className={css.row}>
+                        <img
+                            style={{
+                                borderRadius: '20px',
+                                border: '1px solid black',
+                                padding: '20px',
+                            }}
+                            src={
+                                'https://i0.wp.com/acharyaganesh.com/wp-content/uploads/2024/10/website-post-wealth-astrology.webp?resize=600%2C600&ssl=1'
+                            }
+                            alt=""
+                        />
+                        <p
+                            style={{
+                                border: '1px solid black',
+                                borderRadius: '25px',
+                                padding: '20px',
+                            }}
+                        >
+                            <h2
+                                style={{
+                                    textAlign: 'center',
+                                    color: '#9a5c23',
+                                    paddingBottom: '20px',
+                                }}
+                            >
+                                Wealth Astrology <br />
+                                The Role of Money Attract Symbol
+                            </h2>
+                            <span style={{ fontWeight: '600' }}>
+                                {' '}
+                                Money attraction symbol
+                            </span>{' '}
+                            has always fascinated the human imagination,
+                            functioning as an effective tools in our pursuit of
+                            financial success. Across the globe, various
+                            societies have treasured these symbols,
+                            incorporating them into rituals, art, and daily life
+                            to attract wealth and abundance. Money attraction
+                            symbols are based on a thorough grasp of the human
+                            brain and the universe’s energetic laws.
+                            <br />
+                            <br />
+                            These symbols are more than superstitions; they
+                            serve as focus points for our desires, intentions,
+                            and actions, guiding us toward financial success.
+                            So, do you want to discover How to Attract Money?
+                            Then Ganesh Acharya is your only choice. Their
+                            sigils are powerful symbols constructed for specific
+                            purposes, and prosperity sigils are intended to
+                            attract financial plenty. Creating a sigil involves:
+                            <br />
+                            <br />
+                            <ol style={{ paddingLeft: '20px' }}>
+                                <li>
+                                    Putting your desire into a distinctive
+                                    design.
+                                </li>
+                                <li>Charging it with your own energy</li>
+                                <li>
+                                    placing it somewhere you’ll see it
+                                    frequently encourages your desire for money.
+                                </li>
+                            </ol>
+                        </p>
+                    </div>
+                    <div
+                        className={css.column}
+                        style={{
+                            border: '1px solid black',
+                            borderRadius: '25px',
+                            padding: '20px',
+                        }}
+                    >
+                        <h2>What is our wealth astrology?</h2>
+                        <br />
+                        <h2 style={{ fontWeight: '400' }}>
+                            At Acharya Ganesh, our Wealth Astrology is an
+                            intriguing topic that has captivated the interest of
+                            many individuals. Astrology is an ancient subject
+                            that studies the relationship between celestial
+                            bodies and human behaviour. Astrology predicts a
+                            person’s fortune depending on the positions of the
+                            planets at the moment. Our astrology is the study of
+                            planetary placements and their effects on a person’s
+                            financial situation.
+                        </h2>
+                    </div>
+                    <div className={css.row}>
+                        <div
+                            style={{
+                                border: '1px solid black',
+                                borderRadius: '25px',
+                                padding: '20px',
+                            }}
+                        >
+                            <h2>Second House and Its Ruler</h2>
+                            <br />
+                            <h2 style={{ fontWeight: '400' }}>
+                                At Acharya Ganesh, our Wealth In traditional
+                                astrology, the second house in the birth chart
+                                represents riches, belongings, and financial
+                                assets. The location of the ruler of the second
+                                house, as well as any planets in this house,
+                                reveals an individual’s attitude toward money,
+                                earning capacity, and financial stability.
+                            </h2>
+                        </div>
+                        <div
+                            style={{
+                                border: '1px solid black',
+                                borderRadius: '25px',
+                                padding: '20px',
+                            }}
+                        >
+                            <h2>Jupiter and Venus</h2>
+                            <br />
+                            <h2 style={{ fontWeight: '400' }}>
+                                Jupiter and Venus are frequently seen as benefic
+                                planets in astrology, representing expansion,
+                                abundance, and prosperity. Jupiter and Venus’
+                                positions and aspects in a person’s birth chart
+                                can signify potential for financial success as
+                                well as the ability to attract riches and
+                                resources.
+                            </h2>
+                        </div>
+                    </div>
+                    <div className={css.row}>
+                        <div
+                            style={{
+                                border: '1px solid black',
+                                borderRadius: '25px',
+                                padding: '20px',
+                            }}
+                        >
+                            <h2>Tenth House</h2>
+                            <br />
+                            <h2 style={{ fontWeight: '400' }}>
+                                The Tenth House regulates career, reputation,
+                                and public recognition. A strong and
+                                well-aspected tenth house might indicate
+                                professional success, which is frequently
+                                associated with financial prosperity. We study
+                                the tenth house and its ruler to determine a
+                                person’s potential for career progress and
+                                financial success.
+                            </h2>
+                        </div>
+                        <div
+                            style={{
+                                border: '1px solid black',
+                                borderRadius: '25px',
+                                padding: '20px',
+                            }}
+                        >
+                            <h2>Transits and Progressions</h2>
+                            <br />
+                            <h2 style={{ fontWeight: '400' }}>
+                                We use the movement of planets during transits
+                                and progressions to provide timing-related
+                                insights into financial matters.
+                            </h2>
+                        </div>
+                        <div
+                            style={{
+                                border: '1px solid black',
+                                borderRadius: '25px',
+                                padding: '20px',
+                            }}
+                        >
+                            <h2>Astrological Remedies</h2>
+                            <br />
+                            <h2 style={{ fontWeight: '400' }}>
+                                In addition to predictive analysis, wealth
+                                astrology may prescribe astrological remedies to
+                                help improve financial prospects. Wearing
+                                specific gemstones, performing rituals or
+                                prayers, or making offerings can all be used to
+                                attract positive planetary influences.
+                            </h2>
+                        </div>
+                    </div>
+                    <div
+                        className={css.column}
+                        style={{
+                            padding: '20px',
+                        }}
+                    >
+                        <h2>Our Home Remedies to Attract Money</h2>
+                        <br />
+                        <h2 style={{ fontWeight: '400' }}>
+                            In astrology, there are several home remedies to
+                            attract money to obtain and maintain money. They
+                            include factors such as a prominent Jupiter in the
+                            birth chart, which in astrology is frequently
+                            connected with expansion, growth, abundance, and
+                            luck. We feel that a strong Jupiter placement in a
+                            birth chart indicates the possibility for financial
+                            wealth and good fortune. When the planets’ positions
+                            in these houses are beneficial, you will accumulate
+                            a lot of fortune in life. However, some people do
+                            not have good birth charts. Worry not, we’ve got you
+                            covered. Here are a few cures that some astrologers
+                            believe are beneficial at attracting money.
+                        </h2>
+                    </div>
+                    <div
+                        className={css.row}
+                        style={{
+                            border: '1px solid black',
+                            padding: '25px',
+                            borderRadius: '25px',
+                        }}
+                    >
+                        <ol>
+                            <li>
+                                Offer water to the Sun: Begin by getting up
+                                early and filling a copper vessel with water. As
+                                you pour the water, sing the Gayatri Mantra.
+                                This ceremony is thought to provide good luck,
+                                increase financial stability, and attract
+                                success into your life.
+                            </li>
+                            <br />
+                            <li>
+                                Wear a silver ring : We are able to assist
+                                families and couples in resolving disputes,
+                                enhancing communication, and strengthening their
+                                emotional bonds. Couples and families can learn
+                                how to better support one another and forge
+                                stronger bonds by being aware of one other’s
+                                astrological influences.
+                            </li>
+                            <br />
+                            <li>
+                                Donate food. : Regular acts of kindness,
+                                particularly food donations to those in need,
+                                are an effective treatment. Making these
+                                generous contributions may assist in removing
+                                financial hurdles and bring good fortune,
+                                prosperity, and financial stability into your
+                                life.
+                            </li>
+                        </ol>
+                        <ol>
+                            <li>These courses usually cover:</li>
+                            <li>
+                                The fundamentals of astrology, including the
+                                signs, planets, and associated traits
+                            </li>
+                            <li>
+                                How do these astrological elements impact
+                                financial trends?
+                            </li>
+                            <li>
+                                Using astrological charts to detect possible
+                                market changes
+                            </li>
+                            <li>
+                                In some situations, courses may also go into
+                                astro-mathematics for financial prediction.
+                            </li>
+                        </ol>
+                    </div>
+                    <div>
+                        <h2>What Does</h2>
+                        <h1>Our Financial Astrology Course Offer ?</h1>
+                        <br />
+                        <div className={css.row}>
+                            <p>
+                                Financial astrology is the study of financial
+                                matters through the observation of celestial
+                                movements. After some time trading the markets,
+                                you will see that major trend changes (CITs)
+                                frequently correspond with specific
+                                astro-harmonic cycles. Indeed, there is ample
+                                evidence that celestial cycles are linked to
+                                financial market movements. Acharya Ganesh’s
+                                Financial Astrology courses are designed to
+                                teach you how planetary movements can influence
+                                financial markets and perhaps help you make
+                                investing decisions.
+                            </p>
+                            <ol>
+                                <li>These courses usually cover:</li>
+                                <li>
+                                    The fundamentals of astrology, including the
+                                    signs, planets, and associated traits
+                                </li>
+                                <li>
+                                    How do these astrological elements impact
+                                    financial trends?
+                                </li>
+                                <li>
+                                    Using astrological charts to detect possible
+                                    market changes
+                                </li>
+                                <li>
+                                    In some situations, courses may also go into
+                                    astro-mathematics for financial prediction.
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <Spacer vertical={'72px'} />
             <Footer />
         </PageContainer>
