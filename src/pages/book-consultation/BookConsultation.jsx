d3dc9f39990886d9bf1f2cd70f8f740977d7caa4
Harshit G <harshitguptahg57@gmail.com>
2025-02-11 22:00:53 +0530

    Chnages for contact form component

--- src/pages/book-consultation/BookConsultation.jsx
+++ src/pages/book-consultation/BookConsultation.jsx
@@ -1,53 +1,58 @@
-
-import { PageContainer } from "../../components/page-container/PageContainer";
-import { TopBar } from "../../components/top-bar/TopBar";
+import { PageContainer } from '../../components/page-container/PageContainer';
+import { TopBar } from '../../components/top-bar/TopBar';
 import css from './style.module.css';
 import ImgHeaderBg from '../../assets/book_consultation_header.png';
 import HanishImg from '../../assets/book_consultation_hanish_bagga.png';
-import { Footer } from "../../components/footer/Footer";
-import Testimonial from "../home/components/testimonials/Testimonials";
-import { Spacer } from "../../components/spacer/Spacer";
-import BookingSteps from "./components/step-consultation/StepsConsultation";
-import WhyChooseUs from "./components/why-choose-us/WhyChoose";
-import FAQSection from "./components/faq-section/FaqSection";
-import { InputField } from "../../components/input-field/InputField";
-import { PrimaryButton } from "../../components/primary-button/PrimaryButton";
+import { Footer } from '../../components/footer/Footer';
+import Testimonial from '../home/components/testimonials/Testimonials';
+import { Spacer } from '../../components/spacer/Spacer';
+import BookingSteps from './components/step-consultation/StepsConsultation';
+import WhyChooseUs from './components/why-choose-us/WhyChoose';
+import FAQSection from './components/faq-section/FaqSection';
+import { InputField } from '../../components/input-field/InputField';
+import { PrimaryButton } from '../../components/primary-button/PrimaryButton';
 import GetInTouchBg from '../../assets/book_consultatio_form.png';
-
+import MainContactForm from '../../components/main-contact-form/Contact';
 
 const BookConsultation = () => {
-
     return (
-
-
         <PageContainer className={css.container}>
             <div
                 style={{ backgroundImage: `url(${ImgHeaderBg})` }}
                 className={css.header}
             >
                 <TopBar />
-
             </div>
 
             <div className={css.WhyChooseUs}>
                 <WhyChooseUs />
             </div>
-            < BookingSteps/>
+            <BookingSteps />
             <div className={css.Hanish_info}>
                 <div className={css.row}>
-                <div className={css.about_description_container}>
+                    <div className={css.about_description_container}>
                         <h2>Hanish Bagga</h2>
                         <Spacer vertical={'4px'} />
                         <h4>Founder & Director at Acharya Ganesh</h4>
                         <Spacer vertical={'24px'} />
                         <p>
-                        I’m Hanish Bagga, an astrologer with over 17 years of experience and 40,000+ consultations worldwide. My approach blends deep astrological insight with a genuine commitment to helping you align with your true purpose.
-                        <br/>
-                        <br/>
-                        I believe astrology is not just about predicting the future—it’s about empowering you to make informed, confident decisions. Whether you’re seeking clarity in your career, relationships, or life path, I provide personalized guidance tailored to your unique journey.
-                        <br/>
-                        <br/>  
-                        Let’s unlock the potential the stars have in store for you, and create the life you’re meant to lead.
+                            I’m Hanish Bagga, an astrologer with over 17 years
+                            of experience and 40,000+ consultations worldwide.
+                            My approach blends deep astrological insight with a
+                            genuine commitment to helping you align with your
+                            true purpose.
+                            <br />
+                            <br />
+                            I believe astrology is not just about predicting the
+                            future—it’s about empowering you to make informed,
+                            confident decisions. Whether you’re seeking clarity
+                            in your career, relationships, or life path, I
+                            provide personalized guidance tailored to your
+                            unique journey.
+                            <br />
+                            <br />
+                            Let’s unlock the potential the stars have in store
+                            for you, and create the life you’re meant to lead.
                         </p>
                     </div>
                     <img
@@ -63,23 +68,25 @@ const BookConsultation = () => {
             </div>
             <div className={css.getintouch}>
                 <div className={css.getintouch_container}>
-
-                    <div className={css.getintouch_heading}>
-                        Get In Touch
-                    </div>
+                    <div className={css.getintouch_heading}>Get In Touch</div>
                     <div className={css.getintouch_row}>
                         <div className={css.row_info}>
                             <div className={css.row_info_heading}>
-                            Unsure About Booking? Let Us Help You Make the Right Choice!
+                                Unsure About Booking? Let Us Help You Make the
+                                Right Choice!
                             </div>
                             <div className={css.row_info_heading1}>
-                            We’ll provide all the necessary info and ensure you’re fully confident in your decision.
+                                We’ll provide all the necessary info and ensure
+                                you’re fully confident in your decision.
                             </div>
-                            <div className={css.row_info_heading2}> 
-                            Kindly provide the required details, and our dedicated team will respond with the most suitable service tailored to your needs within 24 hours.
+                            <div className={css.row_info_heading2}>
+                                Kindly provide the required details, and our
+                                dedicated team will respond with the most
+                                suitable service tailored to your needs within
+                                24 hours.
                             </div>
                         </div>
-                        <div className={css.rowform}>
+                        {/* <div className={css.rowform}>
                             <div
                                 style={{ backgroundImage: `url(${GetInTouchBg})` }}
                                 className={css.contact_form}
@@ -114,7 +121,8 @@ const BookConsultation = () => {
                                 </div>
                                 <PrimaryButton>Submit</PrimaryButton>
                             </div>
-                        </div>
+                        </div> */}
+                        <MainContactForm />
                     </div>
                 </div>
             </div>
@@ -123,9 +131,8 @@ const BookConsultation = () => {
             </div>
 
             <Footer />
-
         </PageContainer>
-    )
-}
+    );
+};
 
 export default BookConsultation;
