6ac00b2f72a44e5a962d5eb2af6b266c1589cd99
Harshit G <harshitguptahg57@gmail.com>
2025-04-21 12:15:54 +0530

    seo site url changes

--- src/util/APIHelper.js
+++ src/util/APIHelper.js
@@ -7,7 +7,7 @@ const REACT_APP_API_URL_DEV = 'http://34.131.192.173:4200/api';
 const REACT_APP_API_URL_PROD = 'http://147.93.104.111/:8000/api';
 const REACT_APP_UPLOAD_URL = 'http://34.131.192.173:4200/api';
 
-const getBaseUrl = () =>
+export const getBaseUrl = () =>
     REACT_APP_ENV == 'production'
         ? REACT_APP_API_URL_PROD
         : REACT_APP_ENV == 'development'
