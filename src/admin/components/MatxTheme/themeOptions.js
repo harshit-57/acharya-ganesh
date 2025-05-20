5df09de16287695733ab9e3dd6c3f90bbe6b1cc1
Harshit G <harshitguptahg57@gmail.com>
2025-05-20 23:03:20 +0530

    lazy loading in app.js

--- src/admin/components/MatxTheme/themeOptions.js
+++ src/admin/components/MatxTheme/themeOptions.js
@@ -1,14 +1,13 @@
-import { red } from '@mui/material/colors';
 import { components } from './components';
 
 const themeOptions = {
-  typography: {
-    fontSize: 14,
-    body1: { fontSize: '14px' },
-  },
+    typography: {
+        fontSize: 14,
+        body1: { fontSize: '14px' },
+    },
 
-  status: { danger: red[500] },
-  components: { ...components },
+    status: { danger: '#f44336' },
+    components: { ...components },
 };
 
 export default themeOptions;
